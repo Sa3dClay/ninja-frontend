62b23cc002a09879d1acd7dae4448f9be5ec275a
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-08-03 23:59:18 +0200

    signup and logout custom hooks add logout button with its styles

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const { signup, isLoading, error } = useSignup();
 
-    const handleSignup = (e) => {
+    const handleSignup = async (e) => {
         e.preventDefault();
-        console.log(email, password);
+
+        await signup(email, password);
     };
 
     return (
@@ -29,7 +32,11 @@ const Signup = () => {
                 required
             ></input>
 
-            <button type="submit">Signup</button>
+            <button type="submit" disabled={isLoading}>
+                Signup
+            </button>
+
+            {error && <div className="error">{error}</div>}
         </form>
     );
 };
